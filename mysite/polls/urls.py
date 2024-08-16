c7cb97c2449c51ae6f2823fd7b7e86cd01fbe713
khanhjt <khanhtiendt686@gmail.com>
2024-08-17 00:09:40 +0700

    django

---
+++ mysite/polls/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("list/", views.viewlist, name = 'view_list'),
+    path("", views.index, name="index"),
+]
---
+++ mysite/polls/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from .models import Question
+
+def index(request):
+    myname = "Huu Khanh"
+    taisan = ["dien thoai", "may tinh", "may bay", "nhieu tien"]
+    context = {"name": myname, "taisan": taisan}
+    return render(request, "polls/index.html",context )
+
+def viewlist(request):
+    list_question = Question.objects.all()
+    context  = {"dsquest": list_question}
+    return render(request, "polls/question_list.html", context)
